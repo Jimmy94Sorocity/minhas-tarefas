0f1b8cf27f29029d08bd7e6369e2d6e10db40cfe
Matheus Fré <matheusfre94@gmail.com>
2024-01-16 13:58:36 -0300

    aplicando o redux e criando o reducer

---
+++ src/components/Tarefas/index.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import * as S from './styles'
+
+import * as enums from '../../ultis/enums/Tarefa'
+
+type Props = {
+  titulo: string
+  prioridade: enums.Prioridade
+  status: enums.Status
+  descricao: string
+}
+
+const Tarefa = ({ titulo, prioridade, status, descricao }: Props) => {
+  const [estaEditando, setEstaEditando] = useState(false)
+
+  return (
+    <S.Card>
+      <S.Titulo>{titulo}</S.Titulo>
+      <S.Tag parametro="prioridade" prioridade={prioridade}>
+        {prioridade}
+      </S.Tag>
+      <S.Tag parametro="status" status={status}>
+        {status}
+      </S.Tag>
+      <S.Decricao value={descricao} />
+      <S.BarraAcoes>
+        {estaEditando ? (
+          <>
+            <S.BotaoSalvar>Salvar</S.BotaoSalvar>
+            <S.BotaoCancelarRemover onClick={() => setEstaEditando(false)}>
+              Cancelar
+            </S.BotaoCancelarRemover>
+          </>
+        ) : (
+          <>
+            <S.Botao onClick={() => setEstaEditando(true)}>Editar</S.Botao>
+            <S.BotaoCancelarRemover>Remover</S.BotaoCancelarRemover>
+          </>
+        )}
+      </S.BarraAcoes>
+    </S.Card>
+  )
+}
+
+export default Tarefa
